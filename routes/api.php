2e0d3bc19cd2a32676069fc22306ec88e6e21f9e
Adm-vmalenga <Adm-vmalenga@ulitanz.com>
2024-01-22 09:52:39 +0300

    Passport user login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('v1')->group(function(){
+    Route::prefix('auth')->group(function(){
+        Route::post('login', [AuthController::class, 'login']);
+    });
+
+    Route::group(['middleware' => ['auth:api']], function(){
+        Route::apiResource('users', UserController::class);
+    });
 });
